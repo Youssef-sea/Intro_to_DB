1d8ca315c444e207a847d1add8488e7f4a3f9e5c
Youssef-sea <youssef-laftouhi@hotmail.fr>
2025-06-29 17:35:30 +0100

    week_8

--- task_4.sql
+++ task_4.sql
@@ -3,13 +3,13 @@ USE alx_book_store;
 
 -- Print the full description of the 'books' table
 SELECT
-    column_name,
-    column_type,
-    is_nullable,
-    column_key,
-    column_default,
-    extra
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
 FROM
-    information_schema.columns
+    INFORMATION_SCHEMA.COLUMNS
 WHERE
-    TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
+    TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
